7810a81151abda8bbc497f0e30d3a7e93b0d767f
nastyasolo <125284999+nastyasolo@users.noreply.github.com>
2023-03-12 21:04:37 +0300

    git add.

--- KPOlab1.1/KPOlab1.1/KPOlab1.1.cpp
+++ KPOlab1.1/KPOlab1.1/KPOlab1.1.cpp
@@ -1,1 +1,27 @@
-﻿
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, XYZ[3]{}, x = 0, y = 0, z = 0, perem = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cin >> perem;
+			XYZ[j] = perem;
+		}
+		x += XYZ[0];
+		y += XYZ[1];
+		z += XYZ[2];
+
+	}
+	if (x == 0 && y == 0 && z == 0)
+	{
+		cout << "YES";
+	}
+	else
+	{
+		cout << "NO";
+	}
+}
---
+++ KPOlab1.2/KPOlab1.2/KPOlab1.2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, max = 0;
+    cin >> a >> b >> c;
+    max = a + b + c;
+    if (max < a * b * c) max = a * b * c;
+    if (max < a + b * c) max = a + b * c;
+    if (max < a * b + c) max = a * b + c;
+    if (max < ((a + b) * c)) max = (a + b) * c;
+    if (max < (a * (b + c))) max = a * (b + c);
+    cout << max;
+}
